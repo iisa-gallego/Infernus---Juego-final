b6bd409719b21a71d5c0c6f8019a4bd7074c5278
erika2201 <erikapapamija@gmail.com>
2020-12-09 10:24:03 -0500

    código

---
+++ Infernus/src/Principal.java
@@ -0,0 +1,26 @@
+import processing.core.PApplet;
+
+public class Principal extends PApplet{
+
+	public static void main(String[] args) {
+		PApplet.main("Principal");
+		
+	}
+	
+	@Override
+	public void settings() {
+		size(1200,700);
+	}
+	
+
+	
+	@Override
+	public void setup() {
+	
+	}
+	
+	@Override
+	public void draw() {
+	
+	}
+}
