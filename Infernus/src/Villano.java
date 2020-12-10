a7a84c775cfed1dd0379078eb0185ca8388f32a5
iisa-gallego <isabella.gallego2@correo.icesi.edu.co>
2020-12-10 10:57:50 -0500

    otra vezzzzz

--- Infernus/src/Villano.java
+++ Infernus/src/Villano.java
@@ -19,4 +19,17 @@ public abstract class Villano {
 		x++;
 		y++;	
 	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
 }
